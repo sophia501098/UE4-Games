bc1448d0852766977a863aedd7c8652e599c2753
sophia501098 <318012110@zju.edu.cn>
2020-10-16 17:13:53 +0800

    张宇晴 2020-10-16 hw1
    腾讯游戏客户端公开课第一周作业：
    UE4源码编译+第三人称空项目打包

---
+++ hello/Source/hello/helloGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "helloGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AhelloGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AhelloGameMode();
+};
+
+
+
